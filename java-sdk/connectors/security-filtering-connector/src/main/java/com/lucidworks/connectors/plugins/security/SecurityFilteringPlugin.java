209b48a91500bcc0aa9a6263af4ccce8332f9ca4
Diego Gutierrez <diego.guiterrez@lucidworks.com>
2020-02-12 02:03:07 -0500

    APOLLO-22289 managed fields (#65)
    update security-filtering connector to use managed fields

--- java-sdk/connectors/security-filtering-connector/src/main/java/com/lucidworks/connectors/plugins/security/SecurityFilteringPlugin.java
+++ java-sdk/connectors/security-filtering-connector/src/main/java/com/lucidworks/connectors/plugins/security/SecurityFilteringPlugin.java
@@ -4,13 +4,12 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.lucidworks.connectors.components.generator.RandomContentGenerator;
 import com.lucidworks.connectors.components.generator.impl.DefaultRandomContentGenerator;
-import com.lucidworks.fusion.connector.plugin.api.plugin.ConnectorPlugin;
-import com.lucidworks.fusion.connector.plugin.api.plugin.ConnectorPluginProvider;
 import com.lucidworks.connectors.plugins.security.config.SecurityFilteringConfig;
 import com.lucidworks.connectors.plugins.security.fetcher.SecurityFilteringAccessControlFetcher;
 import com.lucidworks.connectors.plugins.security.fetcher.SecurityFilteringContentFetcher;
-import com.lucidworks.connectors.plugins.security.security.SecurityFilteringSecurityFilterComponent;
 import com.lucidworks.connectors.plugins.security.validation.SecurityFilteringValidationComponent;
+import com.lucidworks.fusion.connector.plugin.api.plugin.ConnectorPlugin;
+import com.lucidworks.fusion.connector.plugin.api.plugin.ConnectorPluginProvider;
 
 import static com.lucidworks.connectors.plugins.security.util.SecurityFilteringConstants.ACCESS_CONTROL;
 import static com.lucidworks.connectors.plugins.security.util.SecurityFilteringConstants.CONTENT;
@@ -31,7 +30,7 @@ public class SecurityFilteringPlugin implements ConnectorPluginProvider {
     return ConnectorPlugin.builder(SecurityFilteringConfig.class)
         .withFetcher(CONTENT, SecurityFilteringContentFetcher.class, fetchModule)
         .withFetcher(ACCESS_CONTROL, SecurityFilteringAccessControlFetcher.class, fetchModule)
-        .withSecurityFilter(SecurityFilteringSecurityFilterComponent.class, fetchModule)
+        .withManagedFilters("AC_SAM_s")
         .withValidator(SecurityFilteringValidationComponent.class, fetchModule)
         .build();
   }
