cef95488778a86424dd2b36a88cafa96613d2a3a
puneet khanal <puneet.khanal@lucidworks.com>
2020-03-06 16:53:38 -0500

    APOLLO-21277: added security filter fields component

---
+++ java-sdk/connectors/security-filtering-connector/src/main/java/com/lucidworks/connectors/plugins/security/component/ExampleSecurityFilterFieldsComponent.java
@@ -0,0 +1,17 @@
+package com.lucidworks.connectors.plugins.security.component;
+
+import com.lucidworks.fusion.connector.plugin.api.security.SecurityFilterFieldsComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleSecurityFilterFieldsComponent implements SecurityFilterFieldsComponent {
+
+  @Override
+  public List<String> getSecurityFilterFields() {
+    List<String> securityFilterFields = new ArrayList<>();
+    securityFilterFields.add("AC_SAM_s");
+    return securityFilterFields;
+  }
+
+}
