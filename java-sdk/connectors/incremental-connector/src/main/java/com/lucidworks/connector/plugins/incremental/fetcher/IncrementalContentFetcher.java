8f81681cd84f828d85fa72eeef3607d4b2331fe3
Cesar Andres <cesar.vera-bernal@lucidworks.com>
2020-05-18 09:16:16 -0500

    APOLLO-25127: no emit solr suffix

--- java-sdk/connectors/incremental-connector/src/main/java/com/lucidworks/connector/plugins/incremental/fetcher/IncrementalContentFetcher.java
+++ java-sdk/connectors/incremental-connector/src/main/java/com/lucidworks/connector/plugins/incremental/fetcher/IncrementalContentFetcher.java
@@ -2,27 +2,26 @@ package com.lucidworks.connector.plugins.incremental.fetcher;
 
 import com.lucidworks.connector.components.generator.RandomContentGenerator;
 import com.lucidworks.connector.components.hostname.HostnameProvider;
+import com.lucidworks.connector.plugins.incremental.config.RandomIncrementalConfig;
 import com.lucidworks.fusion.connector.plugin.api.fetcher.result.FetchResult;
-import com.lucidworks.fusion.connector.plugin.api.fetcher.result.PreFetchResult;
 import com.lucidworks.fusion.connector.plugin.api.fetcher.type.content.ContentFetcher;
 import com.lucidworks.fusion.connector.plugin.api.fetcher.type.content.FetchInput;
-import com.lucidworks.connector.plugins.incremental.config.RandomIncrementalConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IncrementalContentFetcher implements ContentFetcher {
 
   private static final Logger logger = LoggerFactory.getLogger(IncrementalContentFetcher.class);
 
-  private final static String CHECKPOINT_PREFIX = "checkpoint_prefix";
-  private final static String TOTAL_INDEXED = "total_indexed";
+  private static final String CHECKPOINT_PREFIX = "checkpoint_prefix";
+  private static final String TOTAL_INDEXED = "total_indexed";
+  private static final String COUNTER_FIELD = "number";
+  private static final String ERROR_ID = "no-number-this-should-fail";
 
   private final RandomIncrementalConfig incrementalContentConfig;
   private final RandomContentGenerator generator;
@@ -40,17 +39,17 @@ public class IncrementalContentFetcher implements ContentFetcher {
   }
 
   @Override
-  public PreFetchResult preFetch(PreFetchContext preFetchContext) {
-    return preFetchContext.newResult();
-  }
-
-  @Override
   public FetchResult fetch(FetchContext fetchContext) {
     FetchInput input = fetchContext.getFetchInput();
     logger.info("Received FetchInput -> {}", input);
     int totalNumDocs = incrementalContentConfig.properties().getRandomContentProperties().totalNumDocs();
     if (!input.hasId()) {
       generateRandom(fetchContext, 0, totalNumDocs);
+      // Simulating an error item here... because we're emitting an item without a "number",
+      // the fetch() call will attempt to convert the number into a long and throw an exception.
+      // The item should be recorded as an error in the ConnectorJobStatus.
+      fetchContext.newCandidate(ERROR_ID)
+          .emit();
       emitCheckpoint(fetchContext, totalNumDocs);
     } else if (input.getId().equals(CHECKPOINT_PREFIX)) {
       int previousTotal = Integer.parseInt(input.getMetadata().get(TOTAL_INDEXED).toString());
@@ -64,11 +63,32 @@ public class IncrementalContentFetcher implements ContentFetcher {
           previousTotal + incrementalContentConfig.properties().totalNumDocsIncremental()
       );
     } else {
-      long num = (Long) input.getMetadata().get("number");
-      Map<String, Object> fields = getFields(num);
-      fetchContext.newDocument()
-          .fields(f -> f.merge(fields))
-          .emit();
+      try {
+        long num = (Long) input.getMetadata().get(COUNTER_FIELD);
+
+        int min = incrementalContentConfig.properties().getRandomContentProperties().minimumNumberSentences();
+        int max = incrementalContentConfig.properties().getRandomContentProperties().maximumNumberSentences();
+
+        String headline = generator.makeHeadline();
+        String txt = generator.makeRandomText(min, max);
+
+        fetchContext.newDocument()
+            .fields(f -> f.setLong(COUNTER_FIELD, num))
+            .fields(f -> f.setLong("timestamp", Instant.now().toEpochMilli()))
+            .fields(f -> f.setString("headline", headline))
+            .fields(f -> f.setString("hostname", hostname))
+            .fields(f -> f.setString("text_t", txt))
+            .emit();
+      } catch (NullPointerException npe) {
+        if (ERROR_ID.equals(input.getId())) {
+          // Simulating an error item here.
+          logger.error("The following error is expected, as means to demonstrate how errors are emitted");
+          fetchContext.newError(input.getId())
+              .withError("Expected exception")
+              .emit();
+        }
+        throw npe;
+      }
     }
     return fetchContext.newResult();
   }
@@ -77,9 +97,9 @@ public class IncrementalContentFetcher implements ContentFetcher {
     logger.info("Emit checkpoint");
     fetchContext.newCheckpoint(CHECKPOINT_PREFIX)
         .metadata(m -> {
-            m.setInteger(TOTAL_INDEXED, totalNumDocs);
-            m.setLong("lastJobRunDateTime", Instant.now().toEpochMilli());
-            m.setString("hostname", hostname);
+          m.setInteger(TOTAL_INDEXED, totalNumDocs);
+          m.setLong("lastJobRunDateTime", Instant.now().toEpochMilli());
+          m.setString("hostname", hostname);
         })
         .emit();
   }
@@ -88,28 +108,10 @@ public class IncrementalContentFetcher implements ContentFetcher {
     IntStream.range(0, total).asLongStream().forEach(index -> {
       long id = start + index;
       logger.info("Emitting candidate -> number {}", id);
-      Map<String, Object> data = Collections.singletonMap("number", id);
       fetchContext.newCandidate(String.valueOf(id))
-          .metadata(m -> m.merge(data))
+          .metadata(m -> m.setLong(COUNTER_FIELD, id))
           .withTransient(true)
           .emit();
     });
-  }
-
-  private Map<String, Object> getFields(long num) {
-    int min = incrementalContentConfig.properties().getRandomContentProperties().minimumNumberSentences();
-    int max = incrementalContentConfig.properties().getRandomContentProperties().maximumNumberSentences();
-
-    String headline = generator.makeHeadline();
-    String txt = generator.makeRandomText(min, max);
-
-    Map<String, Object> fields = new HashMap<>();
-    fields.put("number_i", num);
-    fields.put("timestamp_l", Instant.now().toEpochMilli());
-    fields.put("headline_s", headline);
-    fields.put("hostname_s", hostname);
-    fields.put("text_t", txt);
-
-    return fields;
   }
 }
